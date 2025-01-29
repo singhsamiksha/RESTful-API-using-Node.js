06540f74b2fa6ee32d3d171a8605cf607f76ac68
singhsamiksha <singhsamiksha877@gmail.com>
2025-01-29 16:22:25 +0530

    screenshots are added

--- Controller/deleteMember.controller.js
+++ Controller/deleteMember.controller.js
@@ -15,7 +15,7 @@ export async function deleteMember(req, res) {
         res.status(200).json({ message: `Member with ID: ${memberId} has been deleted successfully.` });
 
     } catch (error) {
-        console.error("Error deleting member:", error);
+        console.error("Error deleting member:", error.message);
         res.status(500).json({ message: "Internal Server Error", error: error.message });
     }
 }
--- Controller/getIdMember.controller.js
+++ Controller/getIdMember.controller.js
@@ -12,7 +12,7 @@ export async function findMember(req, res) {
         res.status(200).json(member);
         console.log("Member found : " + member);
     } catch (error) {
-        console.error("Error in findMember:", error);
+        console.error("Error in finding the member with this id : ", req.params.id);
         res.status(500).json({ message: "Internal Server Error", error: error.message });
     }
 }
